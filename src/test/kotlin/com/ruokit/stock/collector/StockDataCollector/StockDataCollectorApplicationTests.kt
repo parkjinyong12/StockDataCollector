485ba06478268a66838c84148b35e9f0bf297b7c
jy.park-humaxmobility <jy.park@humaxmobility.co.kr>
2024-09-24 19:05:17 +0900

    Spring Boot 설치

---
+++ src/test/kotlin/com/ruokit/stock/collector/StockDataCollector/StockDataCollectorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ruokit.stock.collector.StockDataCollector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockDataCollectorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
